edc80596543be960da4dc7528f05df6ca077df75
yangzhujun <yangzhujun1@gmail.com>
2016-12-14 15:37:10 +0800

    when created a group, join it automatically again

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,8 +2,13 @@ class GroupsController < ApplicationController
 
   before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
   before_action :find_group_and_check_permission, only: [:edit, :update, :destroy]
+
   def index
     @groups = Group.all
+  end
+
+  def new
+  @group = Group.new
   end
 
   def show
@@ -11,23 +16,19 @@ class GroupsController < ApplicationController
     @posts = @group.posts.recent.paginate(:page => params[:page], :per_page => 5)
   end
 
-def new
-@group = Group.new
-end
-
-def create
-  @group = Group.new(group_params)
-  @group.user = current_user
-  if @group.save
-    current_user.join!(@group)
-    redirect_to groups_path
-  else
-    render :new
+  def edit
   end
-end
 
-def edit
-end
+  def create
+    @group = Group.new(group_params)
+    @group.user = current_user
+    if @group.save
+      current_user.join!(@group)
+      redirect_to groups_path
+    else
+      render :new
+    end
+  end
 
 def update
   if @group.update(group_params)
@@ -42,7 +43,7 @@ def destroy
   redirect_to groups_path, alert: "Group deleted"
 end
 
-def join
+  def join
    @group = Group.find(params[:id])
 
     if !current_user.is_member_of?(@group)
@@ -69,15 +70,17 @@ def join
   end
 
 private
-def find_group_and_check_permission
-    @group = Group.find(params[:id])
 
-    if current_user != @group.user
-      redirect_to root_path, alert: "You have no permission."
-    end
+def find_group_and_check_permission
+  @group = Group.find(params[:id])
+
+  if current_user != @group.user
+    redirect_to root_path, alert: "You have no permission."
   end
+end
+
 def group_params
-   params.require(:group).permit(:title, :description)
+  params.require(:group).permit(:title, :description)
 end
 
 end
